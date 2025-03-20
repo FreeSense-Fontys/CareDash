add02741c92dbbd37bb9ec631d4eb51629a7a9db
MingLi9 <mingjanssen9@gmail.com>
2025-03-20 15:20:08 +0100

    Vitest and Playwright
    Using Vitest for unit testing and Playwright for e2e testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -7,4 +8,21 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    test: {
+        environment: 'jsdom',
+        include: ['src/**/*.test.tsx'],
+        globals: true,
+        coverage: {
+            provider: 'v8', // or 'istanbul'
+            reporter: ['text', 'lcov'],
+            include: ['src/**/*.tsx'], // Specify which folders to cover
+            exclude: ['src/**/*.test.tsx', 'src/**/*.ts', 'src/App.{tsx, ts}', 'src/main.{tsx, ts}'], // Specify which folders to exclude
+            thresholds: {
+                statements: 80,
+                branches: 80,
+                functions: 80,
+                lines: 80,
+            },
+        },
+    }
 })
