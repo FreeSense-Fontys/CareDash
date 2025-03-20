db9d1a9d785df2021a49dff3f91940ee9347885c
Scarletbobcat <tien.hoan314@yahoo.com>
2025-03-20 11:24:21 +0100

    Added error handling to login and reset password forms

--- src/Login/components/LoginForm.tsx
+++ src/Login/components/LoginForm.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import Cookies from 'js-cookie'
 import exh from '../../Auth'
-import { MfaRequiredError } from '@extrahorizon/javascript-sdk'
+import {
+    EmailUnknownError,
+    FieldFormatError,
+    InvalidGrantError,
+} from '@extrahorizon/javascript-sdk'
 import Const from '../../Auth/const'
-import { IoMdInformationCircleOutline } from 'react-icons/io'
+import { GoAlertFill, GoAlert } from 'react-icons/go'
 
 interface LoginProps {
     setAccessToken: (token: string) => void
@@ -11,30 +15,102 @@ interface LoginProps {
 }
 
 function LoginForm({ setAccessToken, setRefreshToken }: LoginProps) {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
     const [isLoading, setIsLoading] = useState(false)
     const [isLogin, setIsLogin] = useState(true)
+
+    // error when login fails
     const [error, setError] = useState('')
 
-    const handleForgotPassword = async () => {
-        try {
-            setIsLoading(true)
-            await exh.users.requestPasswordReset(email)
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setIsLoading(false)
+    // errors for input validation
+    const [inputErrors, setInputErrors] = useState({
+        email: '',
+        password: '',
+    })
+
+    // general input change handler
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.name as keyof typeof formData
+        const value = e.target.value
+        setFormData({ ...formData, [name]: value })
+
+        // removes input error when input changes
+        if (inputErrors[name]) {
+            setInputErrors((prevErrors) => ({
+                ...prevErrors,
+                [name]: '',
+            }))
         }
     }
 
+    // validates the inputs; returns true if valid, false otherwise
+    const validateInputs = () => {
+        const validationErrors = {
+            email: '',
+            password: '',
+            mfa: '',
+        }
+
+        // sets error message for empty fields
+        for (const key in formData) {
+            if (!formData[key as keyof typeof formData]) {
+                validationErrors[key as keyof typeof validationErrors] = `${
+                    key.charAt(0).toUpperCase() + key.slice(1)
+                } is required`
+            }
+        }
+
+        // if there are errors, set the errors and return false
+        if (
+            Object.keys(validationErrors).some(
+                (key) =>
+                    validationErrors[key as keyof typeof validationErrors] !==
+                    ''
+            )
+        ) {
+            setInputErrors(validationErrors)
+            return false
+        }
+        return true
+    }
+
+    // handles form submission: validates inputs and, if inputs valid, logs in user
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (validateInputs()) await UserLogin()
+    }
+
+    // handles forgot password: validates inputs and, if inputs valid, sends password reset email
+    const handleForgotPassword = async () => {
+        if (validateInputs()) {
+            try {
+                setIsLoading(true)
+                setInputErrors({
+                    email: '',
+                    password: '',
+                })
+                await exh.users.requestPasswordReset(formData.email)
+            } catch (error) {
+                if (error instanceof FieldFormatError) setError('Invalid email')
+                if (error instanceof EmailUnknownError)
+                    setError('Email not found')
+            } finally {
+                setIsLoading(false)
+            }
+        }
+    }
+
+    // logs in user
     async function UserLogin() {
         try {
-            setError('')
+            setIsLoading(true)
             await exh.auth
                 .authenticate({
-                    username: email,
-                    password: password,
+                    username: formData.email,
+                    password: formData.password,
                 })
                 .then((user) => {
                     Cookies.set(Const.ACCESS_TOKEN, user.accessToken)
@@ -43,11 +119,11 @@ function LoginForm({ setAccessToken, setRefreshToken }: LoginProps) {
                     setRefreshToken(user.refreshToken)
                 })
         } catch (error) {
-            //handling the multi factor authentication
-            if (error instanceof MfaRequiredError) setError('User requires MFA')
-            else if (error.status === 400)
-                setError('Invalid username or password')
+            if (error instanceof InvalidGrantError)
+                setError('Email or password is incorrect')
             else setError('An unknown error occurred')
+        } finally {
+            setIsLoading(false)
         }
     }
 
@@ -61,30 +137,39 @@ function LoginForm({ setAccessToken, setRefreshToken }: LoginProps) {
             )}
 
             {/* Inputs */}
-            <form
-                onSubmit={async (e) => {
-                    e.preventDefault()
-                    await UserLogin()
-                }}
-            >
+            <form onSubmit={handleSubmit}>
                 <div className="flex flex-col items-center justify-center mt-2">
+                    {/* Error message banner */}
                     {error && (
-                        <div className="bg-red-500 text-white w-full p-2 flex items-center">
-                            <IoMdInformationCircleOutline size={25} />
+                        <div className="bg-red-500 text-white w-full p-4 flex items-center">
+                            <GoAlert size={25} />
                             <p className="pl-2">{error}</p>
                         </div>
                     )}
+
+                    {/* Email input */}
                     <div className="flex flex-col m-2 w-xs">
                         <label htmlFor="email" className="mb-2">
-                            Email
+                            <div className="flex flex-row">
+                                Email
+                                {inputErrors.email !== '' && (
+                                    <p className="text-red-500 pl-2">
+                                        <GoAlertFill size={20} />
+                                    </p>
+                                )}
+                            </div>
                         </label>
                         <input
+                            value={formData.email}
                             id="email"
                             autoComplete="email"
                             type="email"
-                            placeholder="Email"
+                            name="email"
+                            placeholder={
+                                inputErrors.email ? inputErrors.email : 'Email'
+                            }
                             className="bg-white text-black p-4 rounded-xsm focus:outline-2"
-                            onChange={(e) => setEmail(e.target.value)}
+                            onChange={(e) => handleChange(e)}
                             disabled={isLoading}
                         />
                     </div>
@@ -93,20 +178,40 @@ function LoginForm({ setAccessToken, setRefreshToken }: LoginProps) {
                     {isLogin ? (
                         <div className="flex flex-col mt-2 w-xs">
                             <label htmlFor="password" className="mb-2">
-                                Password
+                                <div className="flex flex-row">
+                                    Password
+                                    {inputErrors.password !== '' && (
+                                        <p className="text-red-500 pl-2">
+                                            <GoAlertFill size={20} />
+                                        </p>
+                                    )}
+                                </div>
                             </label>
                             <input
+                                value={formData.password}
                                 id="password"
+                                name="password"
                                 type="password"
-                                placeholder="Password"
+                                placeholder={
+                                    inputErrors.password
+                                        ? inputErrors.password
+                                        : 'Password'
+                                }
                                 className="bg-white text-black p-4 rounded-xsm focus:outline-2"
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => handleChange(e)}
                                 disabled={isLoading}
                             />
+
+                            {/* Forgot Password button */}
                             <p className="text-xs text-left mt-2">
                                 <button
                                     onClick={async (e) => {
                                         e.preventDefault()
+                                        setError('')
+                                        setInputErrors({
+                                            email: '',
+                                            password: '',
+                                        })
                                         setIsLogin(false)
                                     }}
                                     className="underline hover:cursor-pointer hover:text-neutral-300 bg-transparent border-none p-0"
@@ -117,11 +222,19 @@ function LoginForm({ setAccessToken, setRefreshToken }: LoginProps) {
                             </p>
                         </div>
                     ) : (
+                        // Back to login button
                         <div className="flex flex-col mt-2 w-xs">
                             <p>
                                 <button
                                     className="underline text-xs text-left hover:cursor-pointer hover:text-neutral-300"
-                                    onClick={() => setIsLogin(true)}
+                                    onClick={() => {
+                                        setError('')
+                                        setInputErrors({
+                                            email: '',
+                                            password: '',
+                                        })
+                                        setIsLogin(true)
+                                    }}
                                     disabled={isLoading}
                                 >
                                     Back to login
@@ -142,6 +255,7 @@ function LoginForm({ setAccessToken, setRefreshToken }: LoginProps) {
                             Login
                         </button>
                     ) : (
+                        // Reset password button
                         <button
                             className="bg-secondary hover:cursor-pointer w-xs p-4 rounded-tl-xsm rounded-br-xsm hover:scale-105 active:opacity-75"
                             type="submit"
