3f76011e13d4c87a86bd3caee569d119a9bcb8ea
UrteVilimaite <u.vilimaite@student.fontys.nl>
2025-03-31 13:55:16 +0200

    Changes

--- src/Monitor/components/PatientListForm.tsx
+++ src/Monitor/components/PatientListForm.tsx
@@ -19,9 +19,9 @@ const PatientListForm = () => {
                             <option>Filter by Illness</option>
                         </select>
                     </div>
-                    <div className="flex items-center gap-2">
+                    <div className="flex items-center bg-background">
                         <button className="bg-accent text-white px-3 py-1 rounded">&#60;</button>
-                        <span className="font-semibold">25 Feb, 2025</span>
+                        <span className=" font-semibold "> 25 Feb, 2025 </span>
                         <button className="bg-accent text-white px-3 py-1 rounded">&#62;</button>
                     </div>
                 </div>
@@ -53,7 +53,59 @@ const PatientListForm = () => {
                 {mock_patient_data.map((patient) =>
                     patient.carepaths.map((carepath, index) => (
                         <div key={`${patient.id}-${index}`} className="space-y-4 mt-2">
-                            <div className="flex items-center justify-between  p-3 bg-background rounded-xsm relative">
+
+                            {index === 0 ? (
+                                <div className="flex items-center justify-between p-3 bg-background rounded-xsm relative">
+                                    <div className="flex justify-left items-center gap-5 w-50 ml-4">
+                                        <span className={`w-3 h-3 ${patient.online ? "bg-green-500" : "bg-gray-500"} rounded-full`}></span>
+                                        <span className="font-medium">{patient.name}</span>
+                                    </div>
+                                    <div className="flex justify-center items-center gap-2 w-22">
+                                        <span className="italic text-gray-600">{carepath.carepath}</span>
+                                    </div >
+
+                                    <div className="flex justify-around gap-5">
+                                        <div className="flex justify-center items-center"><div className="text-center border size-12 rounded-lg justify-center items-center flex">120</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 bg-red-500 text-white p-2 rounded-lg justify-center items-center flex leading-tight">128 /80</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 bg-red-500 text-white p-2 rounded-lg justify-center items-center flex">80</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 rounded-lg justify-center items-center flex"></div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center border size-12 rounded-lg justify-center items-center flex">20</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 rounded-lg justify-center items-center flex"></div></div>
+                                    </div>
+                                    <div className="flex justify-around">
+                                        <input type="checkbox" className="w-6 h-6 rounded border-gray-300 focus:ring-blue-500 mr-5 " />
+                                    </div>
+                                </div>
+                            ) : null}
+
+                            {index === 1 && patient.carepaths.length > 1 ? (
+                                <div className=" p-3 rounded-xsm relative" >
+                                <div className="flex items-center justify-between p-3 bg-background rounded-xsm relative ml-40">
+                                    <div className="gap-5 w-50 ml-4">
+                                        <span className={`w-3 h-3 `}></span>
+                                        <span className="font-medium"></span>
+                                    </div>
+                                    <div className="flex justify-center items-center gap-2 w-22">
+                                        <span className="italic text-gray-600">{carepath.carepath}</span>
+                                    </div >
+
+                                    <div className="flex justify-around gap-5">
+                                        <div className="flex justify-center items-center"><div className="text-center border size-12 rounded-lg justify-center items-center flex">120</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 bg-red-500 text-white p-2 rounded-lg justify-center items-center flex leading-tight">128 /80</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 bg-red-500 text-white p-2 rounded-lg justify-center items-center flex">80</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 rounded-lg justify-center items-center flex"></div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center border size-12 rounded-lg justify-center items-center flex">20</div></div>
+                                        <div className="flex justify-center items-center"><div className="text-center size-12 rounded-lg justify-center items-center flex"></div></div>
+                                    </div>
+                                    <div className="flex justify-around">
+                                        <input type="checkbox" className="w-6 h-6 rounded  mr-5 " />
+                                    </div>
+                                </div>
+                                </div>
+                            ) : null}
+
+
+                            {/* <div className="flex items-center justify-between  p-3 bg-background rounded-xsm relative">
 
                                 <div className="flex justify-left items-center gap-5 w-50 ml-4">
                                     <span className={`w-3 h-3 ${patient.online ? "bg-green-500" : "bg-gray-500"} rounded-full`}></span>
@@ -74,7 +126,7 @@ const PatientListForm = () => {
                                 <div className="flex justify-around">
                                     <input type="checkbox" className="w-6 h-6 rounded border-gray-300 focus:ring-blue-500 mr-5 " />
                                 </div>
-                            </div>
+                            </div> */}
 
                             {/* <div className="flex items-center justify-between  p-3 bg-blue-100 rounded-xsm relative">
 
