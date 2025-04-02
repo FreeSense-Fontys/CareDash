e8b48bfc52afb8a44f5e126e48894eb3d7bb0bc3
UrteVilimaite <u.vilimaite@student.fontys.nl>
2025-04-02 11:18:36 +0200

    Calendar feature

--- src/Monitor/components/PatientListForm.tsx
+++ src/Monitor/components/PatientListForm.tsx
@@ -1,8 +1,25 @@
 import '../../App.css'
+import { useState, useRef } from 'react'
 import mock_patient_data from './MockPatientData'
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+
 
 
 const PatientListForm = () => {
+
+    const [selectedDate, setSelectedDate] = useState(dayjs());
+    const [open, setOpen] = useState(false);
+    // Date picker opens bellow calendar
+    const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+    const handlePrevDay = () => setSelectedDate((prev) => prev.subtract(1, "day"));
+
+    const handleNextDay = () => setSelectedDate((prev) => prev.add(1, "day"));
+
+
 
     return (
         <div className="bg-gray-100 p-6 ">
@@ -11,31 +28,68 @@ const PatientListForm = () => {
                     {/* Search */}
                     <div className='justify-center items-center'>
                         <div className='text-white'>.....</div>
-                        <input type="text" placeholder="Search..." className="p-2 border rounded w-80" />
+                        <input type="text" placeholder="Search..." className="p-2 border rounded-lg w-80 " />
                     </div>
 
                     <div className="flex gap-4">
-                    {/* Sort */}
+                        {/* Sort */}
                         <div className='justify-center items-center'>
                             <div>Sort by</div>
-                            <select className="p-2 border rounded w-25">
+                            <select className="p-2 border rounded-lg w-25">
                                 <option>Priority</option>
                             </select>
                         </div>
-                    {/* Filter */}
+                        {/* Filter */}
                         <div>
                             <div>Filter by</div>
-                            <select className="p-2 border rounded w-25">
+                            <select className="p-2 border rounded-lg w-25">
                                 <option>Illness</option>
                             </select>
                         </div>
-                    </div>
-                    {/* Calendar */}
-                    <div className="flex items-center bg-background">
-                        <button className="bg-accent text-white px-3 py-1 rounded rounded-l-lg">&#60;</button>
-                        <span className=" font-semibold m-1"> 25 Feb, 2025 </span>
-                        <button className="bg-accent text-white px-3 py-1 rounded rounded-r-lg">&#62;</button>
-                    </div>
+                    </div>                      
+                    {/* Callendar */}
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <div className="flex items-center ">
+                            <button
+                                onClick={handlePrevDay}
+                                className="bg-accent text-white px-3 py-1 rounded-l-lg hover:bg-blue-600"
+                            >
+                                &lt;
+                            </button>
+                            <button
+                                ref={buttonRef}
+                                onClick={() => setOpen(true)}
+                                className="text-black bg-background py-1 p-2"
+                            >
+                                {selectedDate.format("DD MMM, YYYY")}
+                            </button>
+                            <button
+                                onClick={handleNextDay}
+                                className="bg-accent text-white px-3 py-1 rounded-r-lg hover:bg-blue-600"
+                            >
+                                &gt;
+                            </button>
+                        </div>
+                        <DesktopDatePicker
+                            value={selectedDate}
+                            onChange={(newValue) => {
+                                if (newValue) {
+                                    setSelectedDate(newValue);
+                                }
+                                setOpen(false);
+                            }}
+                            open={open}
+                            onClose={() => setOpen(false)}
+                            slotProps={{
+                                textField: { sx: { display: "none" } }, // Hide input field
+                                popper: {
+                                    anchorEl: buttonRef.current, // Attach to button
+                                    placement: "bottom-start", // Open below the button
+                                    disablePortal: true, // Keep inside DOM
+                                },
+                            }}
+                        />
+                    </LocalizationProvider>
                 </div>
 
                 {/* Vitals Header */}
