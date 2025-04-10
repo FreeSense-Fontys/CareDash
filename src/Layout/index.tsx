bfe92cb4d2b394bbfc2f863fd4d9adc444e94b8d
UrteVilimaite <u.vilimaite@student.fontys.nl>
2025-04-10 15:47:17 +0200

    Icons added

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -3,6 +3,10 @@ import { useAuth } from '../contexts/AuthProvider'
 import { Link, useLocation } from 'react-router-dom'
 import img from '../assets/logo.webp'
 import { useNavigate } from 'react-router-dom'
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import { MdDisplaySettings } from "react-icons/md";
+import { LiaNotesMedicalSolid } from "react-icons/lia";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Layout = () => {
     const { user, handleLogout } = useAuth()
@@ -14,42 +18,42 @@ const Layout = () => {
 
     const isActive = (path: string) => location.pathname === path
     return (
-        <div className="flex flex-row">
-            <div className="flex flex-col bg-secondary h-screen w-1/5 justify-between text-white items-center">
-                <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-row ">
+            <div className="flex flex-col bg-secondary h-screen w-1/5 justify-between text-white items-center ">
+                <div className="flex flex-col items-center justify-center ">
                     <img
                         src={img}
                         alt="logo"
                         data-testid="layout-logo"
-                        className="w-5/6 p-4"
+                        className="w-5/6 p-4 mt-3"
                     />
-                    <div className="flex flex-col items-center justify-center space-y-4 pt-14 w-full">
+                    <div className="flex text-center flex-col items-center justify-center space-y-4 pt-10 w-full">
                         <Link
                             to="/"
-                            className={`text-xl truncate text-center w-5/6 p-2 ${
+                            className={` flex text-xl truncate justify-center items-center w-5/6 p-2 gap-2 ${
                                 isActive('/') && activeStyling
                             }`}
                             data-testid="monitor-link"
                         >
-                            Monitor
+                            <MonitorHeartIcon/> Monitor
                         </Link>
                         <Link
                             to="/configuration"
-                            className={`text-xl truncate text-center w-5/6 p-2 ${
+                            className={`flex text-xl truncate justify-center items-center w-5/6 p-2 gap-2 ${
                                 isActive('/configuration') && activeStyling
                             }`}
                             data-testid="configuration-link"
                         >
-                            Configuration
+                            <MdDisplaySettings/> Configuration
                         </Link>
                         <Link
                             to="/presets"
-                            className={`text-xl truncate text-center w-5/6 p-2 ${
+                            className={`flex text-xl truncate justify-center items-center w-5/6 p-2 gap-2 ${
                                 isActive('/presets') && activeStyling
                             }`}
                             data-testid="presets-link"
                         >
-                            Presets
+                            <LiaNotesMedicalSolid/> Presets
                         </Link>
                     </div>
                 </div>
@@ -64,9 +68,9 @@ const Layout = () => {
                             navigate('/login')
                         }}
                         data-testid="logout-button"
-                        className="text-xl truncate bg-neutral-600 w-9/10 rounded-xsm m-2 p-2 cursor-pointer"
+                        className="flex text-xl truncate justify-center gap-2 items-center bg-neutral-600 w-9/10 rounded-xsm m-2 p-2 cursor-pointer"
                     >
-                        Logout
+                        Logout <LogoutIcon/>
                     </button>
                 </div>
             </div>
