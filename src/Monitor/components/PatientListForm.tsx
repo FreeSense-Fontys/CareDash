c4223bd0064991b437a11e88e6f6b47b0b47efc0
MingLi9 <mingjanssen9@gmail.com>
2025-04-10 13:59:49 +0200

    Extract search options
    Make a new component of the search options. pass all these props so it can be reused for the detailed overview with the graphs

--- src/Monitor/components/PatientListForm.tsx
+++ src/Monitor/components/PatientListForm.tsx
@@ -1,95 +1,30 @@
 import '../../App.css'
 import { useState, useRef } from 'react'
 import dayjs from 'dayjs'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
 import PatientList from './PatientList'
+import SearchOptions from './SearchOptions'
 
 const PatientListForm = () => {
     // Date
     const [selectedDate, setSelectedDate] = useState(dayjs())
     const [open, setOpen] = useState(false)
     // Date picker opens bellow calendar
-    const buttonRef = useRef<HTMLButtonElement | null>(null)
     const handlePrevDay = () =>
         setSelectedDate((prev) => prev.subtract(1, 'day'))
     const handleNextDay = () => setSelectedDate((prev) => prev.add(1, 'day'))
+    console.log(selectedDate.format('YYYY-MM-DD'))
+    console.log(open)
 
     return (
         <div className="bg-gray-100 p-6 ">
             <div className="max-w-5xl mx-auto bg-white p-4 rounded-lg shadow">
-                <div className="flex justify-between items-center mb-4">
-                    {/* Search */}
-                    <div className="justify-center items-center">
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            className="p-2 border rounded-lg w-80 "
-                        />
-                    </div>
-
-                    <div className="flex gap-4">
-                        {/* Sort */}
-                        <div className="justify-center items-center">
-                            <div>Sort by</div>
-                            <select className="p-2 border rounded-lg w-25">
-                                <option>Priority</option>
-                            </select>
-                        </div>
-                        {/* Filter */}
-                        <div>
-                            <div>Filter by</div>
-                            <select className="p-2 border rounded-lg w-25">
-                                <option>Illness</option>
-                            </select>
-                        </div>
-                    </div>
-                    {/* Callendar */}
-
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <div className="flex items-center ">
-                            <button
-                                onClick={handlePrevDay}
-                                className="bg-accent text-white px-3 py-2 rounded-l-lg hover:bg-blue-600"
-                            >
-                                &lt;
-                            </button>
-                            <button
-                                ref={buttonRef}
-                                onClick={() => setOpen(true)}
-                                className="text-black bg-background py-2 p-2"
-                            >
-                                {selectedDate.format('DD MMM, YYYY')}
-                            </button>
-                            <button
-                                onClick={handleNextDay}
-                                className="bg-accent text-white px-3 py-2 rounded-r-lg hover:bg-blue-600"
-                            >
-                                &gt;
-                            </button>
-                        </div>
-                        <DesktopDatePicker
-                            value={selectedDate}
-                            onChange={(newValue) => {
-                                if (newValue) {
-                                    setSelectedDate(newValue)
-                                }
-                                setOpen(false)
-                            }}
-                            open={open}
-                            onClose={() => setOpen(false)}
-                            slotProps={{
-                                textField: { sx: { display: 'none' } }, // Hide input field
-                                popper: {
-                                    anchorEl: buttonRef.current, // Attach to button
-                                    placement: 'bottom-start', // Open below the button
-                                    disablePortal: true, // Keep inside DOM
-                                },
-                            }}
-                        />
-                    </LocalizationProvider>
-                </div>
+                <SearchOptions 
+                    selectedDate={selectedDate} 
+                    setSelectedDate={setSelectedDate}
+                    open={open}
+                    setOpen={setOpen}
+                    handlePrevDay={handlePrevDay}
+                    handleNextDay={handleNextDay}/>
 
                 {/* Vitals Header */}
                 <div className="flex justify-end gap-5 text-center font-semibold text-white p-2 rounded pr-18">
